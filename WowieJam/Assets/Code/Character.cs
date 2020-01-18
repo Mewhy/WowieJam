72ded1c2df60e138482518ca9c28a79181eb4d6b
DevZaive <zaivethedev@gmail.com>
2020-01-18 17:39:45 -0500

    Add DeathMechanics

--- WowieJam/Assets/Code/Character.cs
+++ WowieJam/Assets/Code/Character.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour{
+public class Character : MonoBehaviour
+{
     //Movement Variables
     public bool flipped;
     public bool jumped;
@@ -15,10 +16,11 @@ public class Character : MonoBehaviour{
 
     //Game Objects and Components
     public GameObject character;
-    public Rigidbody2D characterRig;
+    private Rigidbody2D characterRig;
     public GameObject mainCamera;
+    public GameObject respawn;
 
-    private void Start(){
+    private void Start() {
         characterRig = character.GetComponent<Rigidbody2D>();
     }
 
@@ -31,62 +33,84 @@ public class Character : MonoBehaviour{
         Vector3 camPos = mainCamera.transform.position;
         charPos.z = -10;
 
-            //Compiling all distances into one number
-            distanceFromPlayer = 0;
-            if ((camPos.x - charPos.x) >= cameraDistance) {
-                distanceFromPlayer = distanceFromPlayer + (camPos.x - charPos.x) - cameraDistance;
-            }
-            if((camPos.x - charPos.x) <= -cameraDistance){
-                distanceFromPlayer = distanceFromPlayer + -(camPos.x - charPos.x) - cameraDistance;
-            }
-            if ((camPos.y - charPos.y) >= cameraDistance){
-                distanceFromPlayer = distanceFromPlayer + (camPos.y - charPos.y) - cameraDistance;
-            }
-            if ((camPos.y - charPos.y) <= -cameraDistance){
-                distanceFromPlayer = distanceFromPlayer + -(camPos.y - charPos.y) - cameraDistance;
-            }
+        //Compiling all distances into one number
+        distanceFromPlayer = 0;
+        if ((camPos.x - charPos.x) >= cameraDistance)
+        {
+            distanceFromPlayer = distanceFromPlayer + (camPos.x - charPos.x) - cameraDistance;
+        }
+        if ((camPos.x - charPos.x) <= -cameraDistance)
+        {
+            distanceFromPlayer = distanceFromPlayer + -(camPos.x - charPos.x) - cameraDistance;
+        }
+        if ((camPos.y - charPos.y) >= cameraDistance)
+        {
+            distanceFromPlayer = distanceFromPlayer + (camPos.y - charPos.y) - cameraDistance;
+        }
+        if ((camPos.y - charPos.y) <= -cameraDistance)
+        {
+            distanceFromPlayer = distanceFromPlayer + -(camPos.y - charPos.y) - cameraDistance;
+        }
 
-            //Lerping that number
-            if (distanceFromPlayer != 0) {
-                mainCamera.transform.position = Vector3.Lerp(camPos, charPos, distanceFromPlayer * Time.deltaTime);
-            }
+        //Lerping that number
+        if (distanceFromPlayer != 0)
+        {
+            mainCamera.transform.position = Vector3.Lerp(camPos, charPos, distanceFromPlayer * Time.deltaTime);
+        }
 
         //Jumping
-        if (Input.GetAxis("Jump") > 0.1 && jumped == false) {
-            if (flipped){
+        if (Input.GetAxis("Jump") > 0.1 && jumped == false)
+        {
+            if (flipped)
+            {
                 characterRig.AddForce(new Vector2(0, jumpSpeed * -1));
             }
-            else {
+            else
+            {
                 characterRig.AddForce(new Vector2(0, jumpSpeed));
             }
             jumped = true;
         }
         //Running
-        if (Input.GetAxis("Fire3") > 0.1) {
+        if (Input.GetAxis("Fire3") > 0.1)
+        {
             movementSpeed = runSpeed;
         }
-        else {
+        else
+        {
             movementSpeed = walkSpeed;
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D col){
-        if (col.gameObject.tag == "Untagged"){
+    private void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "Untagged")
+        {
             jumped = false;
         }
-        if (col.gameObject.tag == "Flip") {
+        if (col.gameObject.tag == "Flip")
+        {
             Flip();
+        }
+        if (col.gameObject.tag == "Death")
+        {
+            Respawn();
         }
     }
 
     void Flip() {
-        if (flipped){
+        if (flipped)
+        {
             flipped = false;
             characterRig.gravityScale = 1;
         }
-        else {
+        else
+        {
             flipped = true;
             characterRig.gravityScale = -1;
         }
+
+    }
+    void Respawn() {
+        character.transform.position = respawn.transform.position;
     }
 }
